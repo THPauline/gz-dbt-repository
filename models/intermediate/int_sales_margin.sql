2827c2dc6b742a57419954e89136cb2089389065
Pau Th <paulinethai289@gmail.com>
2024-10-30 19:17:33 +0000

    name column margin_percent

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -2,7 +2,7 @@ select
     *
     ,ROUND((s.revenue - p.purchse_price),2) AS margin
     ,ROUND((s.quantity * p.purchse_price),2) AS purchase_cost
-    ,{{ margin_percent('s.revenue', 's.quantity * CAST(p.purchse_price AS FLOAT64)') }}
+    ,{{ margin_percent('s.revenue', 's.quantity * CAST(p.purchse_price AS FLOAT64)') }} AS margin_percent
 from {{ref('stg_raw__sales')}} as s
 left join {{ref('stg_raw__product')}} as p
 USING(products_id)
